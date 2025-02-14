43f3741426171d572a409f8b3170861b9b165393
Avinashkr000 <akum97543@gmail.com>
2025-02-14 20:25:15 +0530

    [Avinash kumar] NestedTryCatchBlocks Problem Solved

---
+++ src/nestedtrycatchblocks/NestedTryCatch.java
@@ -0,0 +1,28 @@
+package org.example.Day04.nestedtrycatchblocks;
+
+import java.util.Scanner;
+
+public class NestedTryCatch {
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter an index: ");
+            int index = scanner.nextInt();
+
+            try {
+                System.out.print("Enter a divisor: ");
+                int divisor = scanner.nextInt();
+
+                int result = numbers[index] / divisor;
+                System.out.println("Result: " + result);
+            } catch (ArithmeticException e) {
+                System.out.println("Cannot divide by zero!");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid array index!");
+        }
+    }
+}
