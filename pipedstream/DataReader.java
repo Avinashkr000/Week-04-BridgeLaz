7dfed9cb0d65f9ea81dec52b00b98c0c4ee59ab9
Avinashkr000 <akum97543@gmail.com>
2025-02-14 10:31:10 +0530

    [Avinash kumar] PipedStream Problem Solved

---
+++ pipedstream/DataReader.java
@@ -0,0 +1,25 @@
+package org.example.Day03.pipedstream;
+
+import java.io.*;
+
+public class DataReader extends Thread {
+    private PipedInputStream pipedInputStream;
+
+    public DataReader(PipedInputStream pipedInputStream) {
+        this.pipedInputStream = pipedInputStream;
+    }
+
+    @Override
+    public void run() {
+        try {
+            int data;
+            while ((data = pipedInputStream.read()) != -1) {
+                System.out.print((char) data);
+            }
+            pipedInputStream.close();
+        } catch (IOException e) {
+            System.out.println("Error in DataReader: " + e.getMessage());
+        }
+    }
+}
+
---
+++ pipedstream/Main.java
@@ -0,0 +1,24 @@
+package org.example.Day03.pipedstream;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            PipedOutputStream pipedOutputStream = new PipedOutputStream();
+            PipedInputStream pipedInputStream = new PipedInputStream(pipedOutputStream);
+
+            DataWriter writer = new DataWriter(pipedOutputStream);
+            DataReader reader = new DataReader(pipedInputStream);
+
+            writer.start();
+            reader.start();
+
+            writer.join();
+            reader.join();
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
+
