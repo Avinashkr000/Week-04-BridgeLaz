2121753b9be67eb819735d7654de61b0af6d431b
Avinashkr000 <akum97543@gmail.com>
2025-02-14 20:26:06 +0530

    [Avinash kumar] TryWithResources Problem Solved

---
+++ src/trywithresources/TryWithResources.java
@@ -0,0 +1,15 @@
+package org.example.Day04.trywithresources;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResources {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("E:\\cppp\\try.txt"))) {
+            System.out.println(br.readLine());
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
