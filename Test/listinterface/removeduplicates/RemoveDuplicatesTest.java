f1926771563e05539919183096c37c832ce45d6f
Avinashkr000 <akum97543@gmail.com>
2025-02-14 10:17:56 +0530

    [Avinash kumar] RemoveDuplicates ListInterface Test

---
+++ Test/listinterface/removeduplicates/RemoveDuplicatesTest.java
@@ -0,0 +1,15 @@
+package org.example.Day02.listinterface.removeduplicates;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RemoveDuplicatesTest {
+
+    @Test
+    void testRemoveDuplicates() {
+       
+        assertTrue(true);
+    }
+
+}
