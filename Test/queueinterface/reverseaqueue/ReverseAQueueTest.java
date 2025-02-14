f51f0a6b3aeaafaa8808a9cd9969c6fd235f98aa
Avinashkr000 <akum97543@gmail.com>
2025-02-14 10:24:17 +0530

    [Avinash kumar] ReverseAQueueTest QueueInterface Test

---
+++ Test/queueinterface/reverseaqueue/ReverseAQueueTest.java
@@ -0,0 +1,20 @@
+package org.example.Day02.queueinterface.reverseaqueue;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseAQueueTest {
+    @Test
+    void testReverseAQueue() {
+        assertTrue(true);
+    }
+
+
+    @Test
+    void testReverseAQueue1() {
+
+        assertTrue(true);
+
+    }
+}
