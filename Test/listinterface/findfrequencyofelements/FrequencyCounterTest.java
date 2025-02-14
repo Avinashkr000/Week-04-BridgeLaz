5079c95062353154e5051999115b220dafd5dfc1
Avinashkr000 <akum97543@gmail.com>
2025-02-14 10:17:07 +0530

    [Avinash kumar] FindFrequencyOfElementsTest ListInterface Test

---
+++ Test/listinterface/findfrequencyofelements/FrequencyCounterTest.java
@@ -0,0 +1,13 @@
+package org.example.Day02.listinterface.findfrequencyofelements;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FrequencyCounterTest {
+
+    @Test
+    void testFrequencyCounter() {
+        assertEquals(2, 2);
+    }
+}
