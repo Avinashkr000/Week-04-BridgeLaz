565c28e00776124634507a9eb0b681882ccd3ce0
Avinashkr000 <akum97543@gmail.com>
2025-02-14 20:24:47 +0530

    [Avinash kumar] MultipleCatchBlocks Problem Solved

---
+++ src/multiplecatchblocks/MultipleCatchBlocks.java
@@ -0,0 +1,18 @@
+package org.example.Day04.multiplecatchblocks;
+
+public class MultipleCatchBlocks {
+    public static void main(String[] args) {
+        try {
+            int[] arr = null;
+            int index = 5;
+            if (arr == null) {
+                throw new NullPointerException();
+            }
+            System.out.println("Value at index " + index + ": " + arr[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index!");
+        } catch (NullPointerException e) {
+            System.out.println("Array is not initialized!");
+        }
+    }
+}
