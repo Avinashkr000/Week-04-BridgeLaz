ac6fc7127a9b28bb951e9be4dbe4f0d1d5974178
Avinashkr000 <akum97543@gmail.com>
2025-02-16 16:39:35 +0530

    [Avinash kumar] tesStringMethod JunitTest Problem Solved

---
+++ src/junittesting/basicproblems/teststringutilitymethod/StringUtils.java
@@ -0,0 +1,19 @@
+package org.example.Day05.junittesting.basicproblems.teststringutilitymethod;
+
+public class StringUtils {
+      public static String reverse(String string){
+            if (string == null) return null;
+            return new StringBuilder(string).reverse().toString();
+      }
+      public static boolean isPalindrome(String string) {
+                if (string == null) return false;
+                String reversed = reverse(string);
+                return string.equals(reversed);
+      }
+      public static String toUpperCase(String string) {
+                if (string == null) return null;
+                return string.toUpperCase();
+            }
+        }
+
+
