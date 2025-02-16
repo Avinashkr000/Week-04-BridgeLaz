c7bfca00f418589f986a55c0b7fade87110c2ec5
Avinashkr000 <akum97543@gmail.com>
2025-02-16 16:35:51 +0530

    [Avinash kumar] beforeAndAfterEachAnnotations JunitTest Problem Solved

---
+++ src/junittesting/basicproblems/beforeandaftereachannotations/DatabaseConnection.java
@@ -0,0 +1,19 @@
+package org.example.Day05.junittesting.basicproblems.beforeandaftereachannotations;
+
+public class DatabaseConnection {
+    private boolean connected;
+
+    public void connect() {
+        connected = true;
+        System.out.println("Database Connected");
+    }
+
+    public void disconnect() {
+        connected = false;
+        System.out.println("Database Disconnected");
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+}
