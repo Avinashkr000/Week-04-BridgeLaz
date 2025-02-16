40f9a2ab3f6df2ae187d26ae3d224407bf514fd8
Avinashkr000 <akum97543@gmail.com>
2025-02-16 16:37:32 +0530

    [Avinash kumar] testCalculatorTest JunitTest Problem Solved

---
+++ src/junittesting/basicproblems/testcalculatorclass/Calculator.java
@@ -0,0 +1,18 @@
+package org.example.Day05.junittesting.basicproblems.testcalculatorclass;
+
+public class Calculator {
+    public int add(int a , int b){
+       return a+b;
+    }
+    public int substract(int a , int b){
+        return a-b;
+    }
+    public int multiply(int a , int b){
+        return a*b;
+    }
+    public double divide(int a , int b){
+        return a/b;
+    }
+
+
+}
