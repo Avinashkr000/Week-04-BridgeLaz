10b86916711aa24ed12ffb9e310de4864f97bc96
Avinashkr000 <akum97543@gmail.com>
2025-02-17 13:59:37 +0530

    [Avinash kumar] jsonReprentationGenerator Reflection Problem solved

---
+++ reflection/advancelevel/jsonrepresentationgenerator/Main.java
@@ -0,0 +1,9 @@
+package org.example.Day06.reflection.advancelevel.jsonrepresentationgenerator;
+public class Main {
+
+    public static void main(String[] args) throws IllegalAccessException {
+        Person person = new Person("John Doe", 30);
+        String jsonString = JsonConverter.toJson(person);
+        System.out.println(jsonString);
+    }
+}
---
+++ reflection/advancelevel/jsonrepresentationgenerator/Person.java
@@ -0,0 +1,11 @@
+package org.example.Day06.reflection.advancelevel.jsonrepresentationgenerator;
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
