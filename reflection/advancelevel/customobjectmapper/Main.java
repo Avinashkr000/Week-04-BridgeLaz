44e5acb97fd3ca790ebdb5435c5f33d5840cc9bb
Avinashkr000 <akum97543@gmail.com>
2025-02-17 13:58:42 +0530

    [Avinash kumar] CustomObjectMapper Reflection Problem solved

---
+++ reflection/advancelevel/customobjectmapper/Main.java
@@ -0,0 +1,15 @@
+package org.example.Day06.reflection.advancelevel.customobjectmapper;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("name", "John Doe");
+        properties.put("age", 30);
+
+        Person person = ObjectMapper.toObject(Person.class, properties);
+        System.out.println(person);
+    }
+}
